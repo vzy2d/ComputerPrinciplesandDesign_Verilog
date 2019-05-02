3952b518474f300fcf99ca2bf35a37b941d8aac6
Liger <ngxiao103@126.com>
2019-05-02 17:49:47 +0800

    _lib

--- _lib/MainFileSample.v
+++ _lib/MainFileSample.v
@@ -1,18 +1,6 @@
 module ();
-input ;
-output ;
+    input ;
+    output ;
 
 endmodule
 
-/*
--Notepad++自动补全-
-module endmodule
-assign wire reg
-begin end
-initial always
-function endfunction
-task endtask
-case default endcase
-parameter
-posedge negedge
-*/
--- _lib/TestBenchSample.v
+++ _lib/TestBenchSample.v
@@ -1,41 +1,28 @@
 `timescale 1ns/1ns
 module TB;
-//input
-reg clk;
+	//input
+	reg clk;
 
-Mux8 D1();
-initial
-	begin
-		$dumpfile("test.vcd");
-		$dumpvars(0, TB);
-		$dumpvars(0, D1., D1., D1., D1.);
-	end
-
-always #1 clk = ~clk;
-	
-always @()
-begin
-
-end
-
-initial
+	always #1 clk = ~clk;
+		
+	always @()
 	begin
 
-	
-	$dumpflush; //保存数据
-	$stop; //结束
 	end
+
+	initial
+		begin
+
+		#100
+		$dumpflush; //保存数据
+		$stop; //结束
+		end
+
+	 D1();
+	initial
+		begin
+			$dumpfile("test.vcd");
+			$dumpvars(0, TB);
+			$dumpvars(0, D1);
+		end
 endmodule 
-
-/*
--Notepad++自动补全-
-module endmodule
-assign wire reg
-begin end
-initial always
-function endfunction
-task endtask
-case default endcase
-parameter
-posedge negedge
-*/
